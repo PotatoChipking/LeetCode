1b27f93a4d16d009163f85ce498019958e193d40
PotatoChipking <1873390670@qq.com>
2024-08-28 18:08:48 +0800

    138-func-learn

---
+++ 138/random.go
@@ -0,0 +1,33 @@
+package _38
+
+type Node struct {
+	Val    int
+	Next   *Node
+	Random *Node
+}
+
+func copyRandomList(head *Node) *Node {
+	m := make(map[*Node]*Node)
+
+	var reverse func(n *Node) *Node
+	reverse = func(n *Node) *Node {
+		if n == nil {
+			return nil
+		}
+		if n1, ok := m[n]; ok {
+			return n1
+		}
+
+		n1 := &Node{
+			Val:    n.Val,
+			Next:   nil,
+			Random: nil,
+		}
+		// 递归出口在于复制的某个节点
+		m[n] = n1
+		n1.Next = reverse(n.Next)
+		n1.Random = reverse(n.Random)
+		return n1
+	}
+	return reverse(head)
+}
